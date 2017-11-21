a30f486e392eb94e760b11abb195f2e54960f25f
lpassamano <leighayna.josele@gmail.com>
2017-11-20 21:10:10 -0500

    added Cart#total and cart associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 
+  def total
+    total = 0
+    line_items.each do |line|
+      quantity = line.quantity
+      price = line.item.price
+      subtotal = price * quantity
+      total += subtotal
+    end
+    total
+  end
 end
