940ce4e18434dfc138bc8cd82d2c437729e211c4
lpassamano <leighayna.josele@gmail.com>
2017-11-20 22:06:24 -0500

    added User#current_cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,8 @@
 class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :items, through: :line_items
+  has_many :orders
+  has_many :users, through: :orders 
 
   def total
     total = 0
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_reader :current_cart
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :orders
   has_many :carts, through: :orders
+
+  def current_cart=(cart)
+    @current_cart = cart
+  end
 end
