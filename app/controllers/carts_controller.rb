d4247685ea43cfc2de322ea8ae8ed1d94d9e6607
lpassamano <leighayna.josele@gmail.com>
2017-11-22 10:15:05 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,33 +1,25 @@
 class CartsController < ApplicationController
   def show
-    #raise "in show controller"
     if current_cart
-      #raise "finding cart!"
       @cart = current_cart
     else
       @cart = Cart.find(params[:id])
     end
   end
 
-#<Cart id: 21, status: nil, user_id: 9>
   def checkout
-    #raise "in checkout"
     current_cart.line_items.each do |line|
       line.item.inventory = line.item.inventory - line.quantity
       line.item.save
     end
-    #raise "updated inventory on all items"
 
     @cart = Cart.find(current_cart.id)
     @cart.status = "submitted"
     @cart.save
-    #raise "@cart should be submitted, cart_id 21"
 
     current_user.current_cart = nil
     current_user.save
-    #raise "current_user.current_cart shoudl be nil"
+
     redirect_to cart_path(@cart), { notice: "Your order has successfully been submitted."}
   end
 end
-
-##["Practical Linen Clock: 2, current inventory 54", "Aerodynamic Granite Bag: 1, current inventory 45", "Small Aluminum Coat: 1, current inventory 82", "Heavy Duty Bronze Watch: 1, current inventory 22", "Durable Marble Chair: 1, current inventory 61"]]
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,24 +1,16 @@
 class LineItemsController < ApplicationController
   def create
-    #raise params.inspect
     unless !!current_cart
-      #raise "Creating new cart"
       current_user.current_cart = current_user.carts.create
-      #raise "Created new cart"
     end
-    #raise "creating line item"
+
     line_item = current_user.current_cart.add_item(params[:item_id])
-    #raise "line item created"
     if line_item.save
-      #raise "new line item saved"
       item = Item.find(line_item[:item_id])
-      #raise "after declaration of item"
       current_user.save
-      #raise "after user.save"
       redirect_to cart_path(current_cart), {notice: "#{item.title.capitalize} added to cart!"}
     else
-      #raise "new line item not saved"
-      redirect_to store_path
+      redirect_to store_path, { notice: "There was an error adding item to cart."}
     end
   end
 end
